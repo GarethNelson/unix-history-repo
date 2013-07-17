GarethNelson/unix-history-repo

commit 68699d722bb6edf1b296d4f5dbbdb0e820bfe31f

libc: name passed into __setenv is not necessarily NUL-terminated
That's particularly true when __setenv is called from __merge_environ.

MFC after:	4 days


git-svn-id: svn+ssh://svn.freebsd.org/base/head@253380 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

lib/libc/stdlib/getenv.c
@@ -505,7 +505,7 @@ __setenv(const char *name, size_t nameLen, const char *value, int overwrite)
 		envVars[envNdx].valueSize = valueLen;
 
 		/* Save name of name/value pair. */
-		env = stpcpy(envVars[envNdx].name, name);
+		env = stpncpy(envVars[envNdx].name, name, nameLen);
 		if ((envVars[envNdx].name)[nameLen] != '=')
 			env = stpcpy(env, "=");
 	}

commit 0b7bd5fe1ede5602bb84e3e53d25d751612c1db5

[fix to r253380] __setenv: be explicit about placing '=' after name
This should a regression introduced in r253380 if malloc'ed memory
happens to have '=' at the right place.

Reported by:	ache
Pointyhat to:	me (avg)
MFC after:	1 day
X-MFC with:	r253380

lib/libc/stdlib/getenv.c
@@ -506,8 +506,7 @@ __setenv(const char *name, size_t nameLen, const char *value, int overwrite)
 
 		/* Save name of name/value pair. */
 		env = stpncpy(envVars[envNdx].name, name, nameLen);
-		if ((envVars[envNdx].name)[nameLen] != '=')
-			env = stpcpy(env, "=");
+		*env++ = '=';
 	}
 	else
 		env = envVars[envNdx].value;